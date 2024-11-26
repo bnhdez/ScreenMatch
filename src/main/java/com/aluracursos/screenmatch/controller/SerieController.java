380936ccf8cbb0548333963d48fef4789c865b04
Boris <borisnandez93@gmail.com>
2024-11-25 22:56:12 -0600

    detalle por id

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class SerieController {
     @GetMapping("/lanzamientos")
     public List<SerieDTO> obtenerLanzamientosMasRecientes(){
         return servicio.obtenerLanzamientosMasRecientes();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obtenerPorId(@PathVariable Long id){
+        return servicio.obtenerPorId(id);
     }
 }
--- src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,9 +27,20 @@ public class SerieService {
         return convierteDatos(repository.lanzamientosMasRecientes());
     }
 
+    public SerieDTO obtenerPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(),
+                    s.getGenero(), s.getActores(), s.getSinopsis());
+        }
+        return null;
+    }
+
     public List<SerieDTO> convierteDatos(List<Serie> serie) {
         return serie.stream()
-                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(),
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(),
                         s.getGenero(), s.getActores(), s.getSinopsis()))
                 .collect(Collectors.toList());
     }
